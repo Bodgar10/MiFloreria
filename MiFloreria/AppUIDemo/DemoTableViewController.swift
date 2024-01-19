435afca4b48236f721b6a9d0018754fca2049147
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2024-01-18 18:46:24 -0600

    Creating new DemoUI for our components and views

--- MiFloreria/AppUIDemo/DemoTableViewController.swift
+++ MiFloreria/AppUIDemo/DemoTableViewController.swift
@@ -9,19 +9,28 @@ import UIKit
 
 class DemoTableViewController: UITableViewController {
     
+    var demoViews = [(sectionName: String, views: [UIView])]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        demoViews.append(("Components", []))
+        demoViews.append(("LogInFeature", []))
+        demoViews.append(("FeedFeature", []))
+        demoViews.append(("SearchFeature", []))
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return demoViews.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return demoViews[section].views.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return demoViews[section].sectionName
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- MiFloreria/SceneDelegate.swift
+++ MiFloreria/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-#if DEBUG
+#if DEV
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
@@ -26,7 +26,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.rootViewController = navigation
         self.window = window
         window.makeKeyAndVisible()
-#else
+#elseif DEMOUI
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
