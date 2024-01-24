843c6d424ee1de3665d7a659653ab7f5b7bc2afa
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2024-01-23 19:25:30 -0600

    Añadiendo nuevas cosas sobre la demo

---
+++ MiFloreria/Extensions/Extension+UIControl.swift
@@ -0,0 +1,8 @@
+//
+//  Extension+UIControl.swift
+//  MiFloreria
+//
+//  Created by Bodgar Espinosa Miranda on 18/01/24.
+//
+
+import Foundation
---
+++ MiFloreria/UIComponents/Buttons/PrimaryButton.swift
@@ -0,0 +1,20 @@
+//
+//  PrimaryButton.swift
+//  MiFloreria
+//
+//  Created by Bodgar Espinosa Miranda on 18/01/24.
+//
+
+import UIKit
+
+class PrimaryButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
