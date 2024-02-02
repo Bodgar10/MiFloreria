2fd7ec75ab7a41f964b2d21ae218c5ccb1172ea8
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2024-02-01 19:59:42 -0600

    Creating new extension for the UIColors that we need to the DesignSystem

---
+++ MiFloreria/Extensions/Extension+UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  Extension+UIColor.swift
+//  MiFloreria
+//
+//  Created by Bodgar Espinosa Miranda on 01/02/24.
+//
+
+import UIKit
+
+final class DesignSystem {
+    static var background = UIColor(named: "DSBackground")
+    static var primary = UIColor(named: "DSPrimary")
+}
+
