b7603f902ae1250ab5ffb19228aeecd51a8c8dcc
David Perez <dp3147850@gmail.com>
2024-02-01 20:47:23 -0600

    Se corregieron los comentarios del CR

--- MiFloreria/Sources/LoginFeature/Home/HomeViewController.swift
+++ MiFloreria/Sources/LoginFeature/Home/HomeViewController.swift
@@ -87,7 +87,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .clear
+        view.backgroundColor = UIColor(named:"DSBackground")
         setupUI()
 #if DEV
         setupBinding()
