62b81487bef944104e13f71e9313bfa625108366
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2024-02-06 19:17:15 -0600

    wewwertgy
    dfgff

--- MiFloreria/Sources/LoginFeature/AdditionalInfo/AdditionalInfoSignInViewController.swift
+++ MiFloreria/Sources/LoginFeature/AdditionalInfo/AdditionalInfoSignInViewController.swift
@@ -22,22 +22,61 @@ final class AdditionalInfoSignInViewController: UIViewController {
         return label
     }()
     
+    private let fullNameStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private let nameTextField: DSLineTF = {
+        let view = DSLineTF()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    private let lastNameTextField: DSLineTF = {
+        let view = DSLineTF()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
     // MARK: Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = DesignSystem.background
+        configureTextFields()
         setupUI()
     }
 
     // MARK: Private functions
     
+    private func configureTextFields() {
+        nameTextField.configure(placeholder: "Nombre")
+        lastNameTextField.configure(placeholder: "Apellido")
+    }
+    
     private func setupUI() {
         view.addSubview(titleLabel)
+        view.addSubview(fullNameStackView)
+        
+        fullNameStackView.addArrangedSubview(nameTextField)
+        fullNameStackView.addArrangedSubview(lastNameTextField)
         
         titleLabel
             .pin(.top, to: view.layoutMarginsGuide, spacing: .xLarge)
             .pin(.leading, to: view.layoutMarginsGuide, spacing: .medium)
             .pin(.trailing, to: view.layoutMarginsGuide, spacing: -.medium)
+        
+        fullNameStackView
+            .pinSize(to: CGSize(width: 0, height: 50))
+            .pin(.top, yAnchor: titleLabel.bottomAnchor, offset: 50)
+            .pin(.trailing, to: view, spacing: -.xLarge)
+            .pin(.leading, to: view, spacing: .xLarge)
     }
 }
